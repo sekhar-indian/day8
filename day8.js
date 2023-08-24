67c2fadc29f002773ff683237b2449db0c2da97e
sekhar-indian <munisekharudavalapati1@gmail.com>
2023-08-24 14:48:17 +0530

    day1

---
+++ day8.js
@@ -0,0 +1,26 @@
+// let form=document.getElementById("addForm");
+// let ul=document.getElementById("items");
+// form.addEventListener('submit',addItem);
+
+// function addItem(e){
+//     e.preventDefault();
+    
+//     //get input value
+//     let newitm=document.getElementById('item').value;
+
+//     //create li eliment 
+//     let li=document.createElement('li');
+//     //add class name
+//     li.className='list-group-item';
+//     //add text node 
+//     li.appendChild(document.createTextNode(newitm));
+//     ul.appendChild(li);
+
+   
+
+// }
+
+
+
+
+// console.log(al);
