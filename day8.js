0294ade1ccb469982719a4bd6d31a3b993887643
sekhar-indian <munisekharudavalapati1@gmail.com>
2023-08-25 15:34:30 +0530

    dd

--- day8.js
+++ day8.js
@@ -1,26 +1,29 @@
-// let form=document.getElementById("addForm");
-// let ul=document.getElementById("items");
-// form.addEventListener('submit',addItem);
+let form=document.getElementById("addForm");
+let ul=document.getElementById("items");
+form.addEventListener('submit',addItem);
 
-// function addItem(e){
-//     e.preventDefault();
+function addItem(e){
+    e.preventDefault();
     
-//     //get input value
-//     let newitm=document.getElementById('item').value;
+    //get input value
+    let newitm=document.getElementById('item').value;
 
-//     //create li eliment 
-//     let li=document.createElement('li');
-//     //add class name
-//     li.className='list-group-item';
-//     //add text node 
-//     li.appendChild(document.createTextNode(newitm));
-//     ul.appendChild(li);
+    //create li eliment 
+    let li=document.createElement('li');
+    //add class name
+    li.className='list-group-item';
+    //add text node 
+    li.appendChild(document.createTextNode(newitm));
+     
+    let deleteBtn=document.createElement('button');
+    deleteBtn.className='btnn btn-danger btn-smflaoat-right delete';
+    deletBtn.appendChild(document.createTextNode('x'));
+    
 
    
 
-// }
+}
 
 
 
 
-// console.log(al);
